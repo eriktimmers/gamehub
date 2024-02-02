e18b26144d7124563d97ef76f6f5572007a6b01a
Erik Timmers <erik.timmers@kega.nl>
2024-02-02 15:38:50 +0100

    [MAIN] Filter Platform

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
+import {Platform} from "./hooks/usePlatforms";
+import platformSelector from "./components/PlatformSelector";
 
 function App() {
     const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+    const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null)
 
 
     return (
@@ -33,8 +36,8 @@ function App() {
                 </GridItem>
             </Show>
             <GridItem area='main'>
-                <PlatformSelector />
-                <GameGrid selectedGenre={selectedGenre}/>
+                <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform)}/>
+                <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre}/>
             </GridItem>
         </Grid>
     )
