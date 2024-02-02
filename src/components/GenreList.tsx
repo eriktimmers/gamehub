4b9517b81e9d0b87fc3ae0921795b9ebd720d1ae
Erik Timmers <erik.timmers@kega.nl>
2024-02-02 14:34:35 +0100

    [MAIN] Highlight the selected Genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,10 +2,12 @@ import useGenres, { Genre } from "../hooks/useGenres";
 import {HStack, Image, List, ListItem, Button, Spinner} from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 
-interface Props {onSelectGenre: (genre: Genre) => void;
+interface Props {
+    onSelectGenre: (genre: Genre) => void;
+    selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
     const { data, isLoading, error } = useGenres();
 
     if (isLoading) return <Spinner />;
@@ -22,7 +24,12 @@ const GenreList = ({ onSelectGenre }: Props) => {
                             borderRadius={8}
                             src={getCroppedImageUrl(genre.image_background)}
                         />
-                        <Button onClick={() => {onSelectGenre(genre)}} fontSize='lg' variant='link'>{genre.name}</Button>
+                        <Button
+                            fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
+                            onClick={() => {onSelectGenre(genre)}}
+                            fontSize='lg'
+                            variant='link'
+                        >{genre.name}</Button>
                     </HStack>
                 </ListItem>
             ))}
